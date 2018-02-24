17431233ca4a179bee7d6c82d4af9667a1edd127
hengyi <hengyicai@hotmail.com>
2018-02-24 14:43:26 +0800

    Graph plotting and result file updating

---
+++ graph_plot.py
@@ -0,0 +1,71 @@
+# coding: utf-8
+import csv
+import networkx as nx
+import matplotlib.pyplot as plt
+
+nodes_need_merge = [
+    ('bug', 'type: bug'),
+    ('feature', 'feature request'),
+    ('documentation', 'docs'),
+    ('testing', 'tests')
+]
+
+
+def extract_edges(edges_f, nodes_f):
+    nodes_dic = {}
+    with open(nodes_f, 'rb') as f:
+        csv_reader = csv.reader(f)
+        next(csv_reader)  # skip header
+        for row in csv_reader:
+            nodes_dic[row[0]] = row[1]
+
+    edges_lst = []
+    with open(edges_f, 'rb') as f:
+        csv_reader = csv.reader(f)
+        next(csv_reader)  # skip header
+        for row in csv_reader:
+            source = nodes_dic[row[0]]
+            target = nodes_dic[row[1]]
+            weight = int(row[-1])
+            edges_lst.append((source, target, weight))
+    return edges_lst
+
+
+def construct_graph(edges_data):
+    di_weighted_graph = nx.DiGraph()
+    for edge in edges_data:
+        di_weighted_graph.add_edge(edge[0], edge[1], weight=edge[-1])
+    return di_weighted_graph
+
+
+def merge_nodes(nodes, G):
+    merged_G = G
+    for duplicated_nodes in nodes:
+        merged_G = nx.contracted_nodes(merged_G, duplicated_nodes[0], duplicated_nodes[1])
+    return merged_G
+
+
+def plot_graph(G):
+    pos = nx.spring_layout(G)
+    nx.draw_networkx_nodes(G, pos,
+                           node_color=None, node_size=100)
+    nx.draw_networkx_labels(G, pos)
+    # nx.draw_networkx_edges(G, pos, edgelist=red_edges, edge_color='r', arrows=True)
+    nx.draw_networkx_edges(G, pos, edgelist=G.edges(), arrows=True)
+    plt.show()
+
+
+def main():
+    edges_f = './data/id2id.csv'
+    nodes_f = './data/id2label.csv'
+    merged_G = merge_nodes(
+        nodes_need_merge,
+        construct_graph(
+            extract_edges(edges_f, nodes_f)
+        )
+    )
+    plot_graph(merged_G)
+
+
+if __name__ == '__main__':
+    main()
